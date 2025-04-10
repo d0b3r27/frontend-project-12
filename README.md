3b079454624683d972e3acc4e5a0748b9651545e
d0b3r27 <shish27@yandex.ru>
2025-04-10 23:51:24 +0500

    delete start files, update eslint, Readme, add avatar.jpg

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/d0b3r27/frontend-project-12/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/d0b3r27/frontend-project-12/actions)
+
+### Render
+[https://frontend-project-12-id8x.onrender.com](https://frontend-project-12-id8x.onrender.com)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import avatar from './assets/avatar.jpg';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <html class="h-100" lang="en" data-cbscriptallow="true">
+      <body className="h-100 bg-light">
+        <div className="h-100">
+          <div className="h-100" id="chat">
+            <div className="d-flex flex-column h-100">
+              <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+                <div className="container">
+                  <a className="navbar-brand" href="/">Hexlet Chat</a>
+                </div>
+              </nav>
+              <div className="container-fluid h-100">
+                <div className="row justify-content-center align-content-center h-100">
+                  <div className="col-12 col-md-8 col-xxl-6">
+                    <div className="card shadow-sm">
+                      <div className="card-body row p-5">
+                        <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                          <img src={avatar} className="rounded-circle" alt="Войти"/>
+                        </div>
+                        <form className="col-12 col-md-6 mt-3 mt-md-0">
+                          <h1 className="text-center mb-4">Войти</h1>
+                          <div className="form-floating mb-3">
+                            <input name="username" autocomplete="username" required="" placeholder="Ваш ник" id="username" className="form-control" value=""/>
+                            <label for="username">Ваш ник</label>
+                          </div>
+                          <div className="form-floating mb-4">
+                            <input name="password" autocomplete="current-password" required="" placeholder="Пароль" type="password" id="password" className="form-control" value=""/>
+                            <label className="form-label" for="password">Пароль</label>
+                          </div>
+                          <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
+                        </form>
+                      </div>
+                      <div className="card-footer p-4">
+                        <div className="text-center">
+                          <span>Нет аккаунта?</span> 
+                          <a href="/signup">Регистрация</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="Toastify"></div>
+            </div>
+          </div>
+      </body>
+    </html>
   )
 }
 
